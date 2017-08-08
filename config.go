a5279269b8d3e560ba747de8c53063fa12d5b35a
nange <cctonange@gmail.com>
2017-08-08 18:53:01 +0800

    update flag args

---
+++ config.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type Config struct {
+	Server     string `json:"server"`
+	ServerPort int    `json:"server_port"`
+	LocalPort  int    `json:"local_port"`
+	Password   string `json:"password"`
+	Method     string `json:"method"` // encryption method
+	Auth       bool   `json:"auth"`   // one time auth
+	Timeout    int    `json:"timeout"`
+}
+
+func ParseConfig(path string) (config *Config, err error) {
+	file, err := os.Open(path) // For read access.
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+
+	config = &Config{}
+	if err = json.Unmarshal(data, config); err != nil {
+		err = errors.WithStack(err)
+		return nil, err
+	}
+	readTimeout = time.Duration(config.Timeout) * time.Second
+	if strings.HasSuffix(strings.ToLower(config.Method), "-auth") {
+		config.Method = config.Method[:len(config.Method)-5]
+		config.Auth = true
+	}
+	return
+}
+
+func UpdateConfig(old, ne *Config) {
+	newVal := reflect.ValueOf(ne).Elem()
+	oldVal := reflect.ValueOf(old).Elem()
+
+	for i := 0; i < newVal.NumField(); i++ {
+		newField := newVal.Field(i)
+		oldField := oldVal.Field(i)
+
+		switch newField.Kind() {
+		case reflect.String:
+			s := newField.String()
+			if s != "" {
+				oldField.SetString(s)
+			}
+		case reflect.Int:
+			i := newField.Int()
+			if i != 0 {
+				oldField.SetInt(i)
+			}
+		}
+	}
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,97 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"strings"
 
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{TimestampFormat: "2006-01-02 15:04:05.000"})
+	log.SetLevel(log.InfoLevel)
+}
+
+func PrintVersion() {
+	const version = "Alpha"
+	fmt.Println("easyss version:", version)
+}
+
+func main() {
+	var configFile, cmdLocal string
+	var printVer, debug bool
+	var cmdConfig Config
+
+	flag.BoolVar(&printVer, "version", false, "print version")
+	flag.StringVar(&configFile, "c", "config.json", "specify config file")
+	flag.StringVar(&cmdConfig.Server, "s", "", "server address")
+	flag.StringVar(&cmdLocal, "b", "", "local address, listen only to this address if specified")
+	flag.StringVar(&cmdConfig.Password, "k", "", "password")
+	flag.IntVar(&cmdConfig.ServerPort, "p", 0, "server port")
+	flag.IntVar(&cmdConfig.Timeout, "t", 300, "timeout in seconds")
+	flag.IntVar(&cmdConfig.LocalPort, "l", 0, "local socks5 proxy port")
+	flag.StringVar(&cmdConfig.Method, "m", "", "encryption method, default: aes-256-cfb")
+	flag.BoolVar(&debug, "d", false, "print debug message")
+	flag.BoolVar(&cmdConfig.Auth, "A", false, "one time auth")
+
+	flag.Parse()
+
+	if printVer {
+		PrintVersion()
+		os.Exit(0)
+	}
+
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	if strings.HasSuffix(cmdConfig.Method, "-auth") {
+		log.Debugln("use one time auth")
+		cmdConfig.Method = cmdConfig.Method[:len(cmdConfig.Method)-5]
+		cmdConfig.Auth = true
+	}
+
+	exists, err := FileExists(configFile)
+	if !exists || err != nil {
+		log.Debugf("config file err:%v", err)
+
+		binDir := path.Dir(os.Args[0])
+		configFile = path.Join(binDir, "config.json")
+
+		log.Debugf("config file not found, try config file %s", configFile)
+	}
+
+	config, err := ParseConfig(configFile)
+	if err != nil {
+		config = &cmdConfig
+		if !os.IsNotExist(errors.Cause(err)) {
+			log.Errorf("error reading %s: %+v", configFile, err)
+			os.Exit(1)
+		}
+	} else {
+		UpdateConfig(config, &cmdConfig)
+	}
+
+	if config.Method == "" {
+		config.Method = "aes-256-gcm"
+	}
+
+}
+
+func FileExists(path string) (bool, error) {
+	f, err := os.Stat(path)
+	if err == nil {
+		if stat.Mode()&os.ModeType == 0 {
+			return true, nil
+		}
+		return false, errors.WithStack(errors.New(path + " exists but is not regular file"))
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, errors.WithStack(err)
 }
