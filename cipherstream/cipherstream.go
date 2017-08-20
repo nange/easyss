b8509d4365d5f1057b47dc52203e737c5a8745b6
nange <cctonange@gmail.com>
2017-08-20 22:31:14 +0800

    update cipherstream

--- cipherstream/cipherstream.go
+++ cipherstream/cipherstream.go
@@ -1,7 +1,6 @@
 package cipherstream
 
 import (
-	"bytes"
 	"net"
 
 	"github.com/pkg/errors"
@@ -40,61 +39,41 @@ func New(conn net.Conn, password, method string) (net.Conn, error) {
 }
 
 func (cs *CipherStream) Write(b []byte) (int, error) {
-	log.Debugf("CipherStream Write b:%v, len:%v", b, len(b))
-	buf := make([]byte, bufSize)
-	total := 0
-	r := bytes.NewReader(b)
-	for {
-		n, err := r.Read(buf)
-		if n > 0 {
-			ciphertxt, err := cs.Encrypt(buf[:n])
-			if err != nil {
-				log.Debugf("encrypt buf:%v, err:%+v", buf[:n], err)
-				return 0, err
-			}
-
-			n2, err := cs.Conn.Write(ciphertxt)
-			if err != nil {
-				log.Debugf("conn write ciphertxt:%v, n2:%v, err:%+v", ciphertxt, n2, errors.WithStack(err))
-				return 0, err
-			}
-
-			total += n2
-		}
-		if err != nil {
-			log.Debugf("read buf err:%+v, n:%v", errors.WithStack(err), n)
-			return total, err
-		}
+	ciphertxt, err := cs.Encrypt(b)
+	if err != nil {
+		log.Debugf("encrypt buf:%v, err:%+v", b, err)
+		return 0, err
 	}
 
-	return total, nil
+	n, err := cs.Conn.Write(ciphertxt)
+	if err != nil {
+		log.Debugf("conn write ciphertxt:%v, n:%v, err:%+v", ciphertxt, n, errors.WithStack(err))
+		return n, err
+	}
+	return n, nil
 }
 
 func (cs *CipherStream) Read(b []byte) (int, error) {
-	log.Debugf("CipherStream Read b len:%v", len(b))
-	wb := bytes.NewBuffer(b)
-	buf := make([]byte, bufSize+cs.NonceSize()+cs.Overhead())
-	total := 0
-	for {
-		n, err := cs.Conn.Read(buf)
-		if n > 0 {
-			plaintxt, err := cs.Decrypt(buf[:n])
-			if err != nil {
-				log.Debugf("decrypt buf:%v, err:%+v, n:%v", buf[:n], err, n)
-				return 0, err
-			}
-			n2, err := wb.Write(plaintxt)
-			if err != nil {
-				log.Debugf("write plaintxt:%v, err:%+v, n:%v", plaintxt, errors.WithStack(err), n2)
-				return 0, err
-			}
-			total += n2
-		}
-		if err != nil {
-			log.Debugf("conn read buf, err:%+v, n:%v", errors.WithStack(err), n)
-			return total, err
-		}
-	}
+	cipherbuf := make([]byte, len(b)+cs.NonceSize()+cs.Overhead())
 
+	total := 0
+	n, err := cs.Conn.Read(cipherbuf)
+	if n > 0 {
+		plaintxt, err := cs.Decrypt(cipherbuf[:n])
+		if err != nil {
+			log.Debugf("decrypt buf:%v, err:%+v, n:%v", cipherbuf[:n], err, n)
+			return 0, err
+		}
+		copy(b, plaintxt)
+		total += len(plaintxt)
+	}
+	if err != nil {
+		log.Debugf("conn read buf, err:%+v, n:%v", errors.WithStack(err), n)
+		return total, err
+	}
 	return total, nil
+}
+
+func Copy(dst net.Conn, src net.Conn) (int64, error) {
+	return 0, nil
 }
