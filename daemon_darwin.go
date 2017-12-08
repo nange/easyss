fba002a9027d34b85ede293af978f4d6d3ac1c7a
nange <cctonange@gmail.com>
2017-12-08 15:30:24 +0800

    fix darwin build

--- daemon_darwin.go
+++ daemon_darwin.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"os/exec"
-	"syscall"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -13,7 +12,6 @@ func daemon() {
 		args := os.Args[1:]
 		args = append(args, "-daemon=false")
 		cmd := exec.Command(os.Args[0], args...)
-		//cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 		cmd.Start()
 		log.Info("[PID]:", cmd.Process.Pid)
 		os.Exit(0)
