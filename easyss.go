714fb9ba60024cfb6b7e38958f5793eddf635457
nange <cctonange@gmail.com>
2020-10-11 21:27:49 +0800

    *: update version

--- easyss.go
+++ easyss.go
@@ -15,7 +15,7 @@ import (
 	"github.com/txthinking/socks5"
 )
 
-const version = "1.1.2"
+const version = "1.2.0"
 
 func init() {
 	log.SetFormatter(&log.JSONFormatter{TimestampFormat: "2006-01-02 15:04:05.000"})
