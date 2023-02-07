46ec084d8f0a62a71ef393c542ab885a56245667
nange <cctonange@gmail.com>
2023-02-07 18:48:55 +0800

    fix: util test

--- util/sys_test.go
+++ util/sys_test.go
@@ -8,21 +8,19 @@ import (
 )
 
 func TestSysSupportPowershell(t *testing.T) {
-	s := SysSupportPowershell()
-	if runtime.GOOS == "windows" {
-		assert.True(t, s)
-	} else {
-		assert.False(t, s)
+	if runtime.GOOS != "windows" {
+		t.SkipNow()
 	}
+	s := SysSupportPowershell()
+	assert.True(t, s)
 }
 
 func TestSysPowershellMajorVersion(t *testing.T) {
-	v := SysPowershellMajorVersion()
-	if runtime.GOOS == "windows" {
-		assert.Greater(t, v, 3)
-	} else {
-		assert.Equal(t, 0, v)
+	if runtime.GOOS != "windows" {
+		t.SkipNow()
 	}
+	v := SysPowershellMajorVersion()
+	assert.GreaterOrEqual(t, v, 3)
 }
 
 func TestSysGatewayAndDevice(t *testing.T) {
