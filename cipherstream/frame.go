d51b5019ab491ff074266e7c3b401899c3d8f279
nange <cctonange@gmail.com>
2024-08-05 11:18:47 +0800

    Revert "chore: update padding size"
    This reverts commit b34b3545a7b14bf4037ca6fa3b351f7cb77e7505.

--- cipherstream/frame.go
+++ cipherstream/frame.go
@@ -13,9 +13,9 @@ import (
 
 const (
 	Http2HeaderLen = 9
-	PaddingSize    = 96
-	MaxPaddingSize = 128
-	MinPaddingSize = 32
+	PaddingSize    = 64
+	MaxPaddingSize = 255
+	MinPaddingSize = 64
 )
 
 type FrameType uint8
