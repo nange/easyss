d265ad9782eb85f5de37d0014690e45388e22dc6
nange <cctonange@gmail.com>
2025-10-23 17:28:20 +0800

    optimize: use sync.WaitGroup.Go in go1.25.0

--- remote_udp.go
+++ remote_udp.go
@@ -35,11 +35,8 @@ func (es *EasyServer) remoteUDPHandle(conn net.Conn, addrStr, method string, isD
 	var _tryReuse bool
 
 	wg := sync.WaitGroup{}
-	wg.Add(2)
 	// send
-	go func() {
-		defer wg.Done()
-
+	wg.Go(func() {
 		var buf = bytespool.Get(MaxUDPDataSize)
 		defer bytespool.MustPut(buf)
 		for {
@@ -69,12 +66,10 @@ func (es *EasyServer) remoteUDPHandle(conn net.Conn, addrStr, method string, isD
 			}
 			_ = csStream.SetDeadline(time.Now().Add(es.MaxConnWaitTimeout()))
 		}
-	}()
+	})
 
 	// receive
-	go func() {
-		defer wg.Done()
-
+	wg.Go(func() {
 		var buf = bytespool.Get(MaxUDPDataSize)
 		defer bytespool.MustPut(buf)
 		for {
@@ -90,7 +85,7 @@ func (es *EasyServer) remoteUDPHandle(conn net.Conn, addrStr, method string, isD
 			}
 			_ = csStream.SetDeadline(time.Now().Add(es.MaxConnWaitTimeout()))
 		}
-	}()
+	})
 
 	wg.Wait()
 
