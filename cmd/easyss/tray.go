488bb2d4146caeda7a736fd086d0678d98412de0
nange <cctonange@gmail.com>
2026-02-12 19:17:53 +0800

    chore: update exit time to 3s

--- cmd/easyss/tray.go
+++ cmd/easyss/tray.go
@@ -392,7 +392,7 @@ func (st *SysTray) CloseService() {
 func (st *SysTray) Exit() {
 	go func() {
 		// Ensure that the process can exit completely under any circumstances.
-		time.Sleep(5 * time.Second)
+		time.Sleep(3 * time.Second)
 		log.Info("[SYSTRAY] force exiting...")
 		os.Exit(0)
 	}()
