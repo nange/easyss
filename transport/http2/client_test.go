c4d8325808a10a262565c4063f593df5f51276f0
nange <cctonange@gmail.com>
2026-06-12 14:21:11 +0800

    fix: ensure uTLS transport uses HTTP/2

---
+++ transport/http2/client_test.go
@@ -0,0 +1,45 @@
+package http2
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	utls "github.com/refraction-networking/utls"
+)
+
+func TestUTLSDialUsesHTTP2(t *testing.T) {
+	protoCh := make(chan string, 1)
+	srv := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		protoCh <- r.Proto
+		w.WriteHeader(http.StatusOK)
+	}))
+	srv.EnableHTTP2 = true
+	srv.Config.Protocols = &http.Protocols{}
+	srv.Config.Protocols.SetHTTP2(true)
+	srv.StartTLS()
+	t.Cleanup(srv.Close)
+
+	slot := newSlot(&utls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"h2"},
+	}, time.Second)
+	t.Cleanup(slot.t.CloseIdleConnections)
+
+	req, err := http.NewRequest(http.MethodPost, srv.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := slot.t.RoundTrip(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	_, _ = io.Copy(io.Discard, resp.Body)
+
+	if got := <-protoCh; got != "HTTP/2.0" {
+		t.Fatalf("server got %s, want HTTP/2.0", got)
+	}
+}
