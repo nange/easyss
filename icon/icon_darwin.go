0dcb42ede74d7ac8097584ed5a52550d9c4facab
nange <cctonange@gmail.com>
2026-08-11 14:38:26 +0800

    fix: shrink macOS tray icon logo within 22pt menu bar box

--- icon/icon_darwin.go
+++ icon/icon_darwin.go
@@ -4,7 +4,8 @@ import (
 	_ "embed"
 )
 
-// TrayData is the PNG data used for the tray icon.
+// TrayData is the PNG data used for the tray icon. The logo is scaled down
+// to ~65% of the 44x44 canvas so it doesn't look oversized in the menu bar.
 //
-//go:embed icon_256_256.png
+//go:embed icon_tray_darwin.png
 var TrayData []byte
